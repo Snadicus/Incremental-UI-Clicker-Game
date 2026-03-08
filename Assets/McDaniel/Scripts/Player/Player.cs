75cc45cb6c57c3d445e046767720df5e8ccc42f0
ColinMcD05 <colinmcd05@gmail.com>
2026-03-07 23:39:38 -0500

    Prepped attack button

--- Assets/McDaniel/Scripts/Player/Player.cs
+++ Assets/McDaniel/Scripts/Player/Player.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
+    EnemyLists enemyList;
+
     // Player stats Strength, Agility, Intelligence, Wisdom
     public int[] stats = { 1, 1, 1, 1 };
+
+    public void Awake()
+    {
+        enemyList = GameObject.Find("GameManager").GetComponent<EnemyLists>();
+    }
+
+    public void Attack()
+    {
+        float damage = stats[0];
+        //enemyList.currentEnemies[0].gameObject.GetComponent<BaseEnemy>().EnemyDamage(damage);
+    }
 }
