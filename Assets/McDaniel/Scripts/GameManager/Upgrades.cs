2d68a256168f8549f1c1a5f00af7e96e9327547e
ColinMcD05 <colinmcd05@gmail.com>
2026-03-27 15:33:45 -0400

    Set up abilities and fixed building income

--- Assets/McDaniel/Scripts/GameManager/Upgrades.cs
+++ Assets/McDaniel/Scripts/GameManager/Upgrades.cs
@@ -66,7 +66,7 @@ public class Upgrades : MonoBehaviour
             return;
         }
         currentBuilding.level++;
-        currentBuilding.income += currentBuilding.income + (currentBuilding.level / currentBuilding.income);
+        currentBuilding.income += (currentBuilding.income/currentBuilding.level);
     }
 
     void TeammateUpgrade(string type)
--- Assets/McDaniel/Scripts/Player/PlayerAbilities.cs
+++ Assets/McDaniel/Scripts/Player/PlayerAbilities.cs
@@ -1,16 +1,84 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerAbilities : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Variables
+    #region
+    // Script references
+    EnemySpawner enemyspawner;
+    Player player;
+    public enum AttackTypes
     {
-        
+        physical,
+        magical
     }
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+
+    // Defining abilities class
+    #region
+    public class Ability
     {
-        
+        public string name;
+        public int level;
+        public float mult; // Multiply strenght by x amount
+        public float cooldown; // How long each attack takes to use again. Higher equals longer.
+        float time; // How long it has been ability since pressed. Does not go above cooldown.
+        public AttackTypes attackType;
+        public Player player;
+        public EnemySpawner enemySpawner;
+
+        public void UseAbility()
+        {
+            enemySpawner.EnemyAttacked(player.stats[3] * mult * level);
+            time = cooldown;
+        }
+
+        public IEnumerator DecreaseCooldown()
+        {
+            while (level > 0)
+            {
+                if(time >= 0)
+                {
+                    time -= Time.deltaTime;
+                } 
+            }
+            yield return null;
+        }
     }
+    #endregion
+
+    // Creating abilities
+    #region
+    public List<Ability> abilities;
+
+    void Awake()
+    {
+        abilities = new List<Ability>
+        {
+            new Ability
+            {
+                name = "Magic Missle",
+                level = 0,
+                mult = 2,
+                cooldown = 5,
+                attackType = AttackTypes.magical,
+                player = player,
+                enemySpawner = enemyspawner,
+            },
+            new Ability
+            {
+                name = "Quick Strike",
+                level = 0,
+                mult = 1.25f,
+                cooldown = 2,
+                attackType = AttackTypes.physical,
+                player = player,
+                enemySpawner = enemyspawner
+            }
+        };
+    }
+    #endregion
 }
